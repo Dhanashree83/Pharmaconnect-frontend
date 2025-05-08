3fb4cdf8ae73b97c24692a0e10738a1efdaf0157
Dhanashree83 <dhanashreethakre306@gmail.com>
2025-05-08 21:42:29 +0530

    bug1

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 
 
-const BASE_URL = "http://localhost:8082";
-// const BASE_URL = "  https://ada9-103-155-210-189.ngrok-free.app";
+// const BASE_URL = "http://localhost:8082";
+const BASE_URL = "https://10dc-103-155-210-45.ngrok-free.app";
